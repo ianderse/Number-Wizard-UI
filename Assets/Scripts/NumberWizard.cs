8fc17f4d1599510a62b6c904b8cb1c505ec1919f
Ian Andersen <ianderse@mac.com>
2015-03-22 10:55:58 -0600

    initial commit; unity course project

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	public Text guessText;
+	
+	int max = 1000;
+	int min = 1;
+	int guess;
+	
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+	
+	void StartGame () {
+		max += 1;
+		NextGuess();
+	}
+	
+	void NextGuess () {
+		guess = Random.Range (min,max);
+		guessText.text = guess.ToString();
+	}
+	
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}	
+	
+	public void GuessCorrect() {
+		StartGame();
+	}
+}
